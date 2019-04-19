d7a5920f5f3ecc03ef601fcca3fee67eac0c357d
Marulon <marlongiova2002@gmail.com>
2019-04-19 00:19:53 -0500

    Update Conexion.java

--- Blink/Blink/src/Forms/Forms/Conexion.java
+++ Blink/Blink/src/Forms/Forms/Conexion.java
@@ -8,6 +8,11 @@ import java.sql.*;
 public class Conexion {
     
     public Conexion Datos = new Conexion();
+    private Connection con;
+    private String URL = "";
+    private String Consulta;
+    private PreparedStatement Control;
+    private Statement inicio;
     
     private Conexion (){
         
