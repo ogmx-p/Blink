1c04702e603457ab418e0f2b2d1440f35157ad35
Marulon <marlongiova2002@gmail.com>
2019-04-19 00:25:43 -0500

    Update Conexion.java

--- Blink/Blink/src/Forms/Forms/Conexion.java
+++ Blink/Blink/src/Forms/Forms/Conexion.java
@@ -1,6 +1,8 @@
 
 package Forms.Forms;
+import java.io.File;
 import java.sql.*;
+import javax.swing.JOptionPane;
 /**
  *
  * @author marlon
@@ -9,12 +11,25 @@ public class Conexion {
     
     public Conexion Datos = new Conexion();
     private Connection con;
-    private String URL = "";
+    private String URL = "jdbc:sqlite:C:\\Blink\\DataBase.db";
     private String Consulta;
     private PreparedStatement Control;
     private Statement inicio;
     
     private Conexion (){
         
+    }
+    
+    public void Inicio (){
+        try {
+            File Adress = new File ("C:\\Blink\\");
+            Adress.mkdir();
+            if (con!=null){
+                con.close();
+            }
+            con = DriverManager.getConnection(URL);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
     }
 }
