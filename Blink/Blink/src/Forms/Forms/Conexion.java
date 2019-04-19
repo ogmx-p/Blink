5ff6a3e32254d2b4e815855604f5013b65daa25a
Marulon <marlongiova2002@gmail.com>
2019-04-19 00:05:50 -0500

    Create Conexion.java

---
+++ Blink/Blink/src/Forms/Forms/Conexion.java
@@ -0,0 +1,15 @@
+
+package Forms.Forms;
+import java.sql.*;
+/**
+ *
+ * @author marlon
+ */
+public class Conexion {
+    
+    public Conexion Datos = new Conexion();
+    
+    private Conexion (){
+        
+    }
+}
